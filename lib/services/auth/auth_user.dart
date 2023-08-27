ac57ae4fd8f39eb7f003c5eabcc7e221bce47ae0
sandeep-pani <80412459+sandeep-pani@users.noreply.github.com>
2023-08-27 17:45:04 +0530

    added id

--- lib/services/auth/auth_user.dart
+++ lib/services/auth/auth_user.dart
@@ -3,10 +3,15 @@ import 'package:flutter/foundation.dart';
 
 @immutable
 class AuthUser {
+  final String id;
   final bool isEmailVerified;
   final String? email;
-  const AuthUser({required this.isEmailVerified, required this.email});
+  const AuthUser(
+      {required this.id, required this.isEmailVerified, required this.email});
 
-  factory AuthUser.fromFirebase(User user) =>
-      AuthUser(isEmailVerified: user.emailVerified, email: user.email);
+  factory AuthUser.fromFirebase(User user) => AuthUser(
+        id: user.uid,
+        isEmailVerified: user.emailVerified,
+        email: user.email,
+      );
 }
